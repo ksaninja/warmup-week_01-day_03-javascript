a82622efc2a6060c4eb1ac82f079ecbd4e634d1c
ksaninja <41994885+ksaninja@users.noreply.github.com>
2019-04-23 11:44:20 +0300

    f

---
+++ plingPlangPlong.js
@@ -0,0 +1,25 @@
+function factors(nums)
+{
+let nums = 25
+if (nums % 3 === 0 && nums % 5 ===0 && nums % 7 ===0 )
+{ console.log(factors("pling"+"plang"+"plong")};
+else if (nums % 3 === 0 && nums % 5 ===0  )
+{
+     console.log(factors("pling"+"plang");
+    }
+else if (nums % 3 === 0  )
+{ 
+    console.log(factors("pling");
+}
+else if (nums % 5 === 0  )
+{ 
+    console.log(factors("plang");
+}
+else if (nums % 7 === 0  )
+{ 
+    console.log(factors("plong");
+}
+else  (nums % 3 === 0 && nums % 7 ===0 )
+{
+    console.log(factors("pling"+"plong");
+}
